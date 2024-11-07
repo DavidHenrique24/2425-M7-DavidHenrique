51757b36f9a54bfd5ddda7d4aab2659d974e6bbc
Deivisito <henriquemaciasdavidalejandro@fpllefia.com>
2024-11-07 17:41:49 +0100

    s

---
+++ teoria/index1-includes.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelicula 1</title>
+</head>
+<body>
+    <?php 
+    include_once ('./components/header.php')
+    ?>
+   
+    <main>
+        <ul>
+        <li><a href="index2-includes.php"> Ir a pelis 2</a></li>
+        </ul>
+    </main>
+    <?php 
+    include_once ('./components/footer.php')
+    ?>
+   
+</body>
+</html>
---
+++ teoria/index2-includes.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelicula 2</title>
+</head>
+<body>
+<?php 
+    include_once ('./components/header.php')
+    ?>
+    <main>
+        <ul>
+            <li><a href="index1-includes.php"> Ir a pelis 1</a></li>
+        </ul>
+    </main>
+    <?php 
+    include_once ('./components/footer.php')
+    ?>
+   
+</body>
+</html>
