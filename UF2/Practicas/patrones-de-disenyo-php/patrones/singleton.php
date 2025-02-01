f31acb39dcd22b2c2a54571dc889680246d2937d
Deivisito <henriquemaciasdavidalejandro@fpllefia.com>
2025-02-01 23:46:15 +0000

    Actu importante de Diseño

--- UF2/Practicas/patrones-de-disenyo-php/patrones/singleton.php
+++ UF2/Practicas/patrones-de-disenyo-php/patrones/singleton.php
@@ -1,11 +1,11 @@
 <?php include '../header.php'; ?>
-
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Singlenton</title>
     <link rel="stylesheet" href="../estilos.css">
+
 </head>
 <body>
 <div class="container mt-5">
